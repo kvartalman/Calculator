e7c6350c5e46a244585eb5416a2e34db862638a3
kvartalman <ilyakov@yandex.ru>
2023-05-10 11:17:26 +0400

    Looks like i finally done it!!!!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from re import match
 
 window = Tk()
 window.title('Calculator')
@@ -19,30 +20,41 @@ def change_sign():
         entry.insert(END, digit)
 
 
-def input_digit(symbol):
+def input_sign(symbol):
     elements = entry.get()
     if len(elements) <= 12:
         if symbol not in '/*+-':
+            input_not_operators(symbol)
+        elif symbol in '/*+-' and len(elements) > 0 and elements[-1] != '.':
+            input_operators(symbol)
+
+
+def input_operators(symbol):
+    elements = entry.get()
+    count = 0
+    for i in elements:
+        if i in '/*+-':
+            count += 1
+    if count == 0:
+        entry.insert(END, symbol)
+    elif count == 1:
+        if elements.startswith('-') or (symbol == '-' and elements[-1] in '*/+-'):
             entry.insert(END, symbol)
-        elif symbol in '/*+-' and len(elements) > 0:
-            count = 0
-            for i in elements:
-                if i in '/*+-':
-                    count += 1
-            if count == 0:
-                entry.insert(END, symbol)
-            elif count == 1:
-                if elements.startswith('-') or (symbol == '-' and elements[-1] in '*/+-'):
-                    entry.insert(END, symbol)
-                elif elements[-1].isdigit():
-                    calculate(symbol)
-            elif count == 2:
-                if elements.startswith('-') and symbol == '-' and elements[-1] in '*/+-':
-                    entry.insert(END, symbol)
-                elif elements[-1].isdigit():
-                    calculate(symbol)
-            elif count > 2 and elements.count('-') == 2:
-                calculate(symbol)
+        elif elements[-1].isdigit():
+            calculate(symbol)
+    elif count == 2:
+        if elements.startswith('-') and symbol == '-' and elements[-1] in '*/+-':
+            entry.insert(END, symbol)
+        elif elements[-1].isdigit():
+            calculate(symbol)
+    elif count > 2 and elements.count('-') == 2:
+        calculate(symbol)
+
+
+def input_not_operators(symbol):
+    elements = entry.get()
+    if (symbol == '.' and elements[-1] != '.') or symbol.isdigit():
+        entry.insert(END, symbol)
 
 
 def clear():
@@ -119,49 +131,50 @@ def calc_div(a, b):
 entry = Entry(window, width=13, font=('', 20), justify=RIGHT)
 entry.place(x=0, y=5)
 
-button1 = Button(window, bg='black', fg='yellow', text='1', command=lambda: input_digit('1'))
+
+button1 = Button(window, bg='black', fg='yellow', text='1', command=lambda: input_sign('1'))
 button1.place(x=0, y=100, width=50, height=50)
 
-button2 = Button(window, bg='black', fg='yellow', text='2', command=lambda: input_digit('2'))
+button2 = Button(window, bg='black', fg='yellow', text='2', command=lambda: input_sign('2'))
 button2.place(x=50, y=100, width=50, height=50)
 
-button3 = Button(window, bg='black', fg='yellow', text='3', command=lambda: input_digit('3'))
+button3 = Button(window, bg='black', fg='yellow', text='3', command=lambda: input_sign('3'))
 button3.place(x=100, y=100, width=50, height=50)
 
-button4 = Button(window, bg='black', fg='yellow', text='4', command=lambda: input_digit('4'))
+button4 = Button(window, bg='black', fg='yellow', text='4', command=lambda: input_sign('4'))
 button4.place(x=0, y=150, width=50, height=50)
 
-button5 = Button(window, bg='black', fg='yellow', text='5', command=lambda: input_digit('5'))
+button5 = Button(window, bg='black', fg='yellow', text='5', command=lambda: input_sign('5'))
 button5.place(x=50, y=150, width=50, height=50)
 
-button6 = Button(window, bg='black', fg='yellow', text='6', command=lambda: input_digit('6'))
+button6 = Button(window, bg='black', fg='yellow', text='6', command=lambda: input_sign('6'))
 button6.place(x=100, y=150, width=50, height=50)
 
-button7 = Button(window, bg='black', fg='yellow', text='7', command=lambda: input_digit('7'))
+button7 = Button(window, bg='black', fg='yellow', text='7', command=lambda: input_sign('7'))
 button7.place(x=0, y=200, width=50, height=50)
 
-button8 = Button(window, bg='black', fg='yellow', text='8', command=lambda: input_digit('8'))
+button8 = Button(window, bg='black', fg='yellow', text='8', command=lambda: input_sign('8'))
 button8.place(x=50, y=200, width=50, height=50)
 
-button9 = Button(window, bg='black', fg='yellow', text='9', command=lambda: input_digit('9'))
+button9 = Button(window, bg='black', fg='yellow', text='9', command=lambda: input_sign('9'))
 button9.place(x=100, y=200, width=50, height=50)
 
-button0 = Button(window, bg='black', fg='yellow', text='0', command=lambda: input_digit('0'))
+button0 = Button(window, bg='black', fg='yellow', text='0', command=lambda: input_sign('0'))
 button0.place(x=0, y=250, width=100, height=50)
 
-button_plus = Button(window, bg='black', fg='yellow', text='+', command=lambda: input_digit('+'))
+button_plus = Button(window, bg='black', fg='yellow', text='+', command=lambda: input_sign('+'))
 button_plus.place(x=150, y=200, width=50, height=50)
 
-button_minus = Button(window, bg='black', fg='yellow', text='-', command=lambda: input_digit('-'))
+button_minus = Button(window, bg='black', fg='yellow', text='-', command=lambda: input_sign('-'))
 button_minus.place(x=150, y=150, width=50, height=50)
 
-button_div = Button(window, bg='black', fg='yellow', text='/', command=lambda: input_digit('/'))
+button_div = Button(window, bg='black', fg='yellow', text='/', command=lambda: input_sign('/'))
 button_div.place(x=150, y=50, width=50, height=50)
 
-button_mult = Button(window, bg='black', fg='yellow', text='*', command=lambda: input_digit('*'))
+button_mult = Button(window, bg='black', fg='yellow', text='*', command=lambda: input_sign('*'))
 button_mult.place(x=150, y=100, width=50, height=50)
 
-button_dot = Button(window, bg='black', fg='yellow', text='.', command=lambda: input_digit('.'))
+button_dot = Button(window, bg='black', fg='yellow', text='.', command=lambda: input_sign('.'))
 button_dot.place(x=100, y=250, width=50, height=50)
 
 button_ce = Button(window, bg='black', fg='yellow', text='CE', command=clear)
